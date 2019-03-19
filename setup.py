ed5c326b46b2650e8129e1bdf20b1d2286316660
Michael Dombrowski <mikepi3.14@gmail.com>
2019-03-18 23:19:36 -0700

    Make setup work

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='deploy',
-    version='',
-    packages=['deployer'],
-    url='',
-    license='',
-    author='Michael Dombrowski',
-    author_email='',
-    description=''
+    name="SimpleDeployer",
+    version="1.0.0",
+    url="",
+    license="",
+    author="Michael Dombrowski",
+    entry_points={"console_scripts": ["simpledeployer=deployer.deployer:main"]},
+    install_requires=["gitpython>=2.1.11", "pyyaml>=5.1"],
+    packages=find_packages(),
+    author_email="",
+    description="",
 )

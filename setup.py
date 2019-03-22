2db6427025ce725a83e8010565999e5f40b9ab38
Michael Dombrowski <mikepi3.14@gmail.com>
2019-03-21 21:48:41 -0700

    Add info

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="SimpleDeployer",
     version="1.0.0",
-    url="",
-    license="",
+    url="https://github.com/mikedombo/simpledeployer",
+    license="BSD-3-Clause",
     author="Michael Dombrowski",
     entry_points={"console_scripts": ["simpledeployer=deployer.deployer:main"]},
     install_requires=["gitpython>=2.1.11", "pyyaml>=5.1"],
     packages=find_packages(),
-    author_email="",
-    description="",
+    author_email="michael@mikedombrowski.com",
+    description="Dead simple deployment system",
 )

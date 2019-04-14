cd67220f388432c48109d2243e19575c1ca128fc
Michael Dombrowski <mikepi3.14@gmail.com>
2019-04-13 17:39:00 -0700

    Update version, add long description

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="SimpleDeployer",
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/mikedombo/simpledeployer",
     license="BSD-3-Clause",
     author="Michael Dombrowski",
     entry_points={"console_scripts": ["simpledeployer=deployer.deployer:main"]},
-    install_requires=["gitpython>=2.1.11", "pyyaml>=5.1"],
+    install_requires=["gitpython>=2.1.11", "pyyaml>=5.1", "click>=7.0"],
     packages=find_packages(),
     author_email="michael@mikedombrowski.com",
     description="Dead simple deployment system",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
